5a531d6a12da1da1ae516c44e294fa19a29f89c9
Tiago Calogero <tiaguinhor@gmail.com>
2019-06-21 09:39:30 +1000

    Start robot video and render images with imagick/gm

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const robots = {
   input: require('./robots/input'),
   text: require('./robots/text'),
   state: require('./robots/state'),
-  image: require('./robots/image')
+  image: require('./robots/image'),
+  video: require('./robots/video')
 };
 
 async function start() {
-  // robots.input();
-  // await robots.text();
+  robots.input();
+  await robots.text();
   await robots.image();
+  await robots.video();
 }
 
 start();
